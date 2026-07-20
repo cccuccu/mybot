5a87da86825600444827c1bf5a2694630ffd1cec
cccuccu <bs7786281@gmail.com>
2026-07-20 09:14:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+From aiogram import Bot, Dispatcher, executor, types
+
 
 TOKEN = "8598589369:AAHVOMO2AfeHxpfQn_1PgtH1l4hKASlCGzs"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("أهلاً بك في البوت! 👋")
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-app = Application.builder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("أهلاً بك، هذا بوت الرسائل")
 
-app.run_polling
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer("وصلت رسالتك: " + message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
